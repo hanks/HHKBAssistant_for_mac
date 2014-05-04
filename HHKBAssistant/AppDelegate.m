00ad7daac64a6c19fa1d10770f0ecc76c4c8bc30
hanks <zhouhan315@gmail.com>
2014-05-04 12:02:10 +0900

    basic ui

--- HHKBAssistant/AppDelegate.m
+++ HHKBAssistant/AppDelegate.m
@@ -7,12 +7,207 @@
 //
 
 #import "AppDelegate.h"
+#import <IOKit/kext/KextManager.h>
+#import <ServiceManagement/ServiceManagement.h>
+
+#define BUILD_IN_KEYBOARD_ENABLE 1
+#define BUILD_IN_KEYBOARD_DISABLE 0
 
 @implementation AppDelegate
 
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification
 {
     // Insert code here to initialize your application
+    self.kbKextIdentifier = @"com.apple.driver.AppleUSBTCKeyboard";
+    NSDictionary *kexts = (__bridge NSDictionary *)KextManagerCopyLoadedKextInfo((__bridge CFArrayRef)[NSArray arrayWithObject: self.kbKextIdentifier], NULL);
+    
+    
+    for (id key in kexts) {
+        NSLog(@"%@: %@", key, kexts[key]);
+    }
+    
+//    myAuthRef = [self authenticateForKbKext];
+    
+    
+//    char myToolPath[] = "/sbin/kextload";
+//    char *myArguments[] = { [kexts[@"OSBundlePath"] UTF8String], NULL };
+//    FILE *myCommunicationsPipe = NULL;
+//    char myReadBuffer[128];
+//    
+//    AuthorizationFlags myFlags = kAuthorizationFlagDefaults;                          // 8
+//    OSStatus myStatus = AuthorizationExecuteWithPrivileges                  // 9
+//    (myAuthRef, myToolPath, myFlags, myArguments, &myCommunicationsPipe);
+//    
+//    NSLog(@"%d\n", myStatus);
+//    NSLog(@"%d", kOSReturnSuccess);
+//    
+//    if (myAuthRef == NULL) {
+//        [self freeAuthRef:myAuthRef];
+//    }
+    
 }
 
+- (AuthorizationRef) authenticateForKbKext {
+    // get authorization
+    AuthorizationRef myAuthorizationRef;
+    OSStatus myStatus;
+    myStatus = AuthorizationCreate(NULL, kAuthorizationEmptyEnvironment,
+                                   kAuthorizationFlagDefaults, &myAuthorizationRef);
+    // ask for just one auth
+    AuthorizationItem myItems[1];
+    
+    // kext auth
+    myItems[0].name = [self.kbKextIdentifier UTF8String];
+    myItems[0].valueLength = 0;
+    myItems[0].value = NULL;
+    myItems[0].flags = 0;
+    
+    AuthorizationRights myRights;
+    myRights.count = sizeof (myItems) / sizeof (myItems[0]);
+    myRights.items = myItems;
+    
+    AuthorizationFlags myFlags;
+    myFlags = kAuthorizationFlagDefaults |
+    kAuthorizationFlagInteractionAllowed |
+    kAuthorizationFlagExtendRights;
+    
+    myStatus = AuthorizationCopyRights(myAuthorizationRef, &myRights,
+                                       kAuthorizationEmptyEnvironment, myFlags, NULL);
+    return myAuthorizationRef;
+}
+
+- (void) freeAuthRef: (AuthorizationRef)authRef {
+    // free authrization
+    AuthorizationFree (authRef,
+                       kAuthorizationFlagDestroyRights);
+    
+}
+
+
+
+- (void)awakeFromNib {
+    // add status icon to system menu bar
+    statusItem = [[NSStatusBar systemStatusBar] statusItemWithLength:NSVariableStatusItemLength];
+    [statusItem setMenu:_statusMenu];
+    
+    // set status bar icon
+    NSString *iconPath = [[NSBundle mainBundle] pathForResource:@"icon_16x16" ofType:@"png"];
+    NSImage *icon = [[NSImage alloc] initWithContentsOfFile:iconPath];
+    [statusItem setImage:icon];
+    [statusItem setToolTip:@"USB Keyboard Helpler"];
+    [statusItem setHighlightMode:YES];
+    
+    // init keyboard change menu title
+    self.kbStatus = BUILD_IN_KEYBOARD_DISABLE;
+    [self setKbChangeMenuTitle:self.kbStatus];
+}
+
+- (void)setKbChangeMenuTitle:(BOOL)kbStatus {
+    switch (kbStatus) {
+        case BUILD_IN_KEYBOARD_DISABLE:
+            [self.kbChangeMenu setTitle:@"Enable Mac build-in keyboard"];
+            break;
+        case BUILD_IN_KEYBOARD_ENABLE:
+            [self.kbChangeMenu setTitle:@"Disable Mac build-in keyboard"];
+            break;
+    }
+}
+
+- (BOOL) getKbStatus {
+    return BUILD_IN_KEYBOARD_ENABLE;
+}
+
+- (void)changeKeyboardWith:(BOOL)kbStatus {
+    switch (kbStatus) {
+        case BUILD_IN_KEYBOARD_ENABLE:
+            // enable build in keyboard
+            
+            break;
+        case BUILD_IN_KEYBOARD_DISABLE:
+            // disable build in keyboard
+            
+            break;
+    }
+    
+    [self setKbChangeMenuTitle:kbStatus];
+}
+
+- (IBAction)changeKeyboardMode:(id)sender {
+    self.kbStatus = !self.kbStatus;
+    [self changeKeyboardWith:self.kbStatus];
+}
+
+- (IBAction)quit:(id)sender {
+    [[NSApplication sharedApplication] terminate:nil];
+}
+
+- (IBAction)runOnStartup:(id)sender {
+    [sender setState:![sender state]];
+    
+    if ([sender state]) {
+        // if true to add app to login item lists
+        [self addAppAsLoginItem];
+    } else {
+        // delete from login item lists
+        [self deleteAppFromLoginItem];
+    }
+}
+
+-(void) addAppAsLoginItem{
+	NSString * appPath = [[NSBundle mainBundle] bundlePath];
+    
+	// This will retrieve the path for the application
+	// For example, /Applications/test.app
+	CFURLRef url = (CFURLRef)CFBridgingRetain([NSURL fileURLWithPath:appPath]);
+    
+	// Create a reference to the shared file list.
+    // We are adding it to the current user only.
+    // If we want to add it all users, use
+    // kLSSharedFileListGlobalLoginItems instead of
+    //kLSSharedFileListSessionLoginItems
+	LSSharedFileListRef loginItems = LSSharedFileListCreate(NULL,
+                                                            kLSSharedFileListSessionLoginItems, NULL);
+	if (loginItems) {
+		//Insert an item to the list.
+		LSSharedFileListItemRef item = LSSharedFileListInsertItemURL(loginItems,
+                                                                     kLSSharedFileListItemLast, NULL, NULL,
+                                                                     url, NULL, NULL);
+		if (item){
+			CFRelease(item);
+        }
+	}
+    
+	CFRelease(loginItems);
+}
+
+-(void) deleteAppFromLoginItem{
+	NSString * appPath = [[NSBundle mainBundle] bundlePath];
+    
+	// This will retrieve the path for the application
+	// For example, /Applications/test.app
+	CFURLRef url = (CFURLRef)CFBridgingRetain([NSURL fileURLWithPath:appPath]);
+    
+	// Create a reference to the shared file list.
+	LSSharedFileListRef loginItems = LSSharedFileListCreate(NULL,
+                                                            kLSSharedFileListSessionLoginItems, NULL);
+    
+	if (loginItems) {
+		UInt32 seedValue;
+		//Retrieve the list of Login Items and cast them to
+		// a NSArray so that it will be easier to iterate.
+		NSArray  *loginItemsArray = (NSArray *)CFBridgingRelease(LSSharedFileListCopySnapshot(loginItems, &seedValue));
+		for(int i = 0 ; i< [loginItemsArray count]; i++){
+			LSSharedFileListItemRef itemRef = (LSSharedFileListItemRef)CFBridgingRetain([loginItemsArray
+                                                                                         objectAtIndex:i]);
+			//Resolve the item with URL
+			if (LSSharedFileListItemResolve(itemRef, 0, (CFURLRef*) &url, NULL) == noErr) {
+				NSString * urlPath = [(NSURL*)CFBridgingRelease(url) path];
+				if ([urlPath compare:appPath] == NSOrderedSame){
+					LSSharedFileListItemRemove(loginItems,itemRef);
+				}
+			}
+		}
+	}
+}
 @end
+
