7ae5f11b0f1c279477174666aafacd1a90c0c2f0
hanks <zhouhan315@gmail.com>
2014-05-06 00:26:33 +0900

    abstract data source protocol to a new file

--- HHKBAssistant/PreferencePaneWindowController.h
+++ HHKBAssistant/PreferencePaneWindowController.h
@@ -7,36 +7,9 @@
 //
 
 #import <Cocoa/Cocoa.h>
+#import "DataSourceDelegate.h"
 
 #define XIBNAME @"prefpane"
-
-// define data source operation deleaget
-@protocol DataSourceDelegate <NSObject>
-
-- (void) addDevice:(NSString *)deviceName;
-- (void) removeDevice:(NSString *)deviceName;
-
-- (NSMutableArray*) getDeviceArr;
-
-- (NSInteger) isAutoDisable;
-- (void) setAutoDisable:(NSInteger)flag;
-
-- (NSInteger) isEnableVoice;
-- (void) setEnableVoice:(NSInteger)flag;
-
-- (NSInteger) isInMsgEnable;
-- (void) setInMsgEnable:(NSInteger)flag;
-
-- (NSInteger) isOutMsgEnable;
-- (void) setOutMsgEnable:(NSInteger)flag;
-
-- (NSString *) getInMsg;
-- (void) setInMsg:(NSString *)msg;
-
-- (NSString *) getOutMsg;
-- (void) setOutMsg:(NSString *)msg;
-
-@end
 
 @interface PreferencePaneWindowController : NSWindowController
 
