fc67d2e02a8c52616e92acd0b2acb1c7f2b4ee03
zhou han <han.zhou@gu3.co.jp>
2014-05-06 18:55:34 +0900

    start to implement disable keyboard helper target

---
+++ DisableKeyboardHelper/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  DisableKeyboardHelper
+//
+//  Created by 周 涵 on 2014/05/06.
+//  Copyright (c) 2014年 hanks. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
